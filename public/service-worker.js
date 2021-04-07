95c6b95eb95b30a9dddb9b2faa4fc0a16b7f3b77
pikulatucker@gmail.com <pikulatucker@gmail.com>
2021-04-06 21:49:49 -0700

    code

---
+++ public/service-worker.js
@@ -0,0 +1,28 @@
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open('blog-cache-v1')
+    .then(cache => cache.addAll([
+      '/',
+      '/index.html',
+      '/index.js',
+      '/db.js',
+      '/icons/icon-192x192.png',
+      '/icons/icon-512x512.png',
+      '/styles.css'
+    ])))
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request)
+    .catch(err => {
+      if (err) { console.log(err) }
+
+      return caches.match(event.request)
+        .then(match => {
+          if (match) {
+            return match
+          } else if (event.request.headers.get('accept').includes('text/html')) {
+            return caches.match('/')
+          }
+        })
+    }))
+})
